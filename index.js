f1c6c36029cc57069f055a191af22f912cdfe7a4
juan <jkpuchipuchi@gmail.com>
2023-11-14 18:03:14 +0100

    Añadir productos

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.use(express.json())  // IMPORTANTE: SOPORTE PARA JSON
 const PORT = process.env.PORT ?? 3001   //Tiene el valor de de la variable de entorno process
 const DB_URL = process.env.MONGODB_URI ?? 'mongodb://localhost:27017'   //Toma la URI de mongo db
 const DB_NAME = process.env.DB_NAME ?? 'api'
-const COLLECTION = 'users'
+const COLLECTION = 'productos'
 
 const client = new MongoClient(DB_URL)
 
 app.get("/", (request, response) => {
-    response.redirect("/api/users")
+    response.redirect("/api/productos")
 })
 
 
 // GET
-app.get('/api/users', async (request, response) => {
+app.get('/api/productos', async (request, response) => {
     const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
@@ -28,7 +28,7 @@ app.get('/api/users', async (request, response) => {
 })
 
 // POST 
-app.post('/api/users', async (request, response) => {
+app.post('/api/productos', async (request, response) => {
     if (!request.is('json'))
         return response.json({ message: 'Debes proporcionar datos JSON' })
 
@@ -36,13 +36,13 @@ app.post('/api/users', async (request, response) => {
     const collection = database.collection(COLLECTION);
 
     const { nombre, edad } = request.body
-    const results = await collection.insertOne({ nombre, edad });
+    const results = await collection.insertOne({ nombre, precio });
 
     return response.status(200).json(results)
 })
 
 // GET 
-app.get('/api/users/:id', async (request, response) => {
+app.get('/api/productos/:id', async (request, response) => {
     const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
@@ -53,7 +53,7 @@ app.get('/api/users/:id', async (request, response) => {
 })
 
 // PUT
-app.put('/api/users/:id', async (request, response) => {
+app.put('/api/productos/:id', async (request, response) => {
     if (!request.is('json'))
         return response.json({ message: 'Debes proporcionar datos JSON' })
 
@@ -62,13 +62,13 @@ app.put('/api/users/:id', async (request, response) => {
 
     const { id } = request.params
     const { nombre, edad } = request.body
-    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { nombre, edad } });
+    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { nombre, precio } });
 
     response.status(200).json(results)
 })
 
 // DELETE
-app.delete('/api/users/:id', async (request, response) => {
+app.delete('/api/productos/:id', async (request, response) => {
     const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
@@ -78,5 +78,5 @@ app.delete('/api/users/:id', async (request, response) => {
 })
 
 
-app.listen(PORT, () => console.log(`Servidor web iniciado en puerto ${PORT}`))
+app.listen(PORT, () => console.log(`OK -> Servidor web iniciado en puerto ${PORT}`))
 
